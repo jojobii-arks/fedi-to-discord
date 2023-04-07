59dc41d40f237eb4e225fc366b0382ba5341bb5d
jojobii <jojobii.arks@gmail.com>
2023-04-06 17:50:49 -0700

    change: only post content

--- src/lib/utils/discord.ts
+++ src/lib/utils/discord.ts
@@ -6,7 +6,7 @@ export async function postToDiscordWebhook(
   const response = await fetch(webhookUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ content, username: `Fediverse Bridge: ${name}` }),
+    body: JSON.stringify({ content }),
   });
   if (!response.ok) {
     throw new Error(
