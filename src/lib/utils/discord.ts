97394d6348c2de1580c95f003c1fe50240a82dea
jojobii <jojobii.arks@gmail.com>
2023-04-06 17:13:22 -0700

    fix: actually add name to username

--- src/lib/utils/discord.ts
+++ src/lib/utils/discord.ts
@@ -6,7 +6,7 @@ export async function postToDiscordWebhook(
   const response = await fetch(webhookUrl, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ content, username: "Fediverse Bridge" }),
+    body: JSON.stringify({ content, username: `Fediverse Bridge: ${name}` }),
   });
   if (!response.ok) {
     throw new Error(
