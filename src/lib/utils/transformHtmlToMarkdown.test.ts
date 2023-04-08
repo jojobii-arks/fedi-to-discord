c81ce40306d35f1b4734a7baaa57077f61a8e63f
jojobii <jojobii.arks@gmail.com>
2023-04-07 19:24:30 -0700

    fix: br to output proper newlines

--- src/lib/utils/transformHtmlToMarkdown.test.ts
+++ src/lib/utils/transformHtmlToMarkdown.test.ts
@@ -2,7 +2,9 @@ import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts";
 import transformHtmlToMarkdown from "./transformHtmlToMarkdown.ts";
 Deno.test("transformHtmlToMarkdown", () => {
   assertEquals(
-    transformHtmlToMarkdown("<p>foo</p>"),
-    "foo",
+    transformHtmlToMarkdown(
+      "<p>Cleansed Unit Lv.2</p><p>Potency +18%<br />Prevents knockback<br />Follow-up attack will occur up to 5 times when you hit an enemy (15 sec cooldown)</p>",
+    ),
+    "Cleansed Unit Lv.2\n\nPotency +18%\nPrevents knockback\nFollow-up attack will occur up to 5 times when you hit an enemy (15 sec cooldown)",
   );
 });
--- src/lib/utils/transformHtmlToMarkdown.ts
+++ src/lib/utils/transformHtmlToMarkdown.ts
@@ -1,6 +1,13 @@
 import TurndownService from "turndown";
 
-const turndownService = new TurndownService();
+const turndownService = new TurndownService()
+  // Make `br` tags into `"\n"`, not `" \n"` (which is the default)
+  .addRule("br", {
+    filter: ["br"],
+    replacement: () => {
+      return "\n";
+    },
+  });
 
 export default function transformHtmlToMarkdown(html: string) {
   return turndownService.turndown(html);
